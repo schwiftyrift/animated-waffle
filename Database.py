51091ed3027523551e1ffc9080f99ff7a8f1adea
Jared Newton <jareddanielnewton@gmail.com>
2025-04-06 21:54:54 -0500

    v0.1.1
    Added second photo taking ability within GUI, saving images is now organized into an images folder. Database adjusted to fit changes

--- Database.py
+++ Database.py
@@ -1,62 +1,21 @@
 import json
 import os
 from random import randint
-'''
-# Path to the JSON file where item history is saved
-item_history_file = "item_history.json"
-
-def load_item_history():
-    """Load item history from the JSON file."""
-    if os.path.exists(item_history_file):
-        with open(item_history_file, "r") as file:
-            return json.load(file)
-    else:
-        print("No history file found.")
-        return []
-
-# Load the item history from the JSON file
-item_history = load_item_history()
-
-# Print all items from the history
-if item_history:
-    print("\nAccessing item history:")
-    for item_data in item_history:
-        print(f"Item: {item_data['label']}, ID: {item_data['id']}")
-else:
-    print("No items detected yet.")
-
-def find_item_by_id(item_id):
-    for item in item_history:
-        if item["id"] == item_id:
-            return item
-    return None
-
-# Find an item by its ID
-item_id_to_search = input("Please enter the id number of the item you want to look up: ")
-found_item = find_item_by_id(item_id_to_search)
-
-if found_item:
-    print(f"Item found: {found_item}")
-else:
-    print(f"Item with ID {item_id_to_search} not found.")
-
-def save_item_history(item_history):
-    """Save the updated item history to the JSON file."""
-    with open(item_history_file, "w") as file:
-        json.dump(item_history, file, indent=4)
-
-save_item_history(item_history)
-'''
 
 # Base class
 class Item:
-    def __init__(self, label, color, description = "No description provided", location = ""):
+    def __init__(self, label, color, secondImage, description = "No description provided", location = ""):
         self.id = self.generate_id()
         self.label = label
         self.color = color
         self.description = description
         self.location = location
-        self.image = f"{self.id + "(1)"}.jpg"
+        self.image = f"images/{self.id}(1).jpg"
+        if secondImage:
+            self.secondImage = f"images/{self.id}(2).jpg"
+        else:
+            self.secondImage = "None"
+
 
     @property
     def id(self):
@@ -126,13 +85,13 @@ def save_item_history(history):
         json.dump(history, file, indent=4)
 
 # Main function
-def inputData(label, color, description, location):
+def inputData(label, color, description, location, secondImage):
     label = label.lower()
     if label not in item_classes:
         raise ValueError(f"Unknown item label: {label}")
 
     item_class = item_classes[label]
-    item_instance = item_class(label, color, description, location)
+    item_instance = item_class(label, color, description, location, secondImage)
 
     itemID = item_instance.id
 
